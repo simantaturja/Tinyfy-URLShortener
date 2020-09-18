77a60d626b90a01805f8253af093f76b0f186981
Simanta Deb Turja <simantaturja@gmail.com>
2020-09-19 01:24:33 +0600

    first release

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tinyfy - A free URL shortener
+
+A free url shortener developed using Mongo, Express, React and Node (MERN stack). This app is currently running a beta version.
+
+
+## Screenshots
+![screenshot of the app]()
---
+++ client/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import './App.css';
+import SearchBar from "material-ui-search-bar";
+import axios from 'axios';
+import ParticlesBg from 'particles-bg';
+
+function App() {
+  const [searchValue, setSearchValue] = useState('');
+  const onChangeSearch = query => setSearchValue(query);
+
+  const [value, setValue] = useState('');
+  
+
+  return (
+    <div className="App">
+      <div className = "particles">
+        <ParticlesBg type = "circle" bg = {true}  num = {100} />
+      </div>
+      <div className="container">
+        <header className="App-header">
+          <h1> Tinyfy </h1>
+        </header>
+        <div className="Subtitle-area">
+          <h4>Shorten your URL!</h4>
+        </div>
+        <div className="Search-area">
+
+          <SearchBar className='Search-bar' placeholder='Paste your url here' onChange={onChangeSearch} value={searchValue}
+            onRequestSearch={() => {
+              try {
+                console.log(searchValue);
+                const response = axios.post('https://tiinify.herokuapp.com/url', { url: searchValue });
+                console.log('👉 Returned data:', response.then((res) => {
+                  if (res.status == 200) {
+                    console.log(res.data.slug);
+                    setValue('https://tiinify.herokuapp.com/'+res.data.slug);
+                  } else {
+                    console.log('error');
+                  }
+                }));
+               
+              } catch (e) {
+                console.log(`😱 Axios request failed: ${searchValue}`);
+              }
+            }}
+          />
+          <card>
+          <h3><a href={value} target="_blank">{value}</a></h3>
+          </card>
+
+          
+        </div>
+        
+
+      </div>
+      <footer>
+        <p>Made with ❤️  by Turja</p>
+      </footer>
+    </div>
+
+  );
+}
+
+
+
+
+
+export default App;
